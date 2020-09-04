3c4940511dd9f0ef5233387ec30a60a2c6412df9
mirob70 <70144025+mirob70@users.noreply.github.com>
2020-09-04 11:49:46 +0200

    Projektdateien hinzufügen.

---
+++ MeinRechner/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeinRechner
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+       // User Story "Addieren": Als Benutzer möchte ich zwei zahlen eingeben, um deren Summe berechnen zu lassen
+       Console.Write("Bitte gib den ersten Summanden ein: ");
+         string ersterSummand = Console.ReadLine();
+
+         Console.Write("Bitte gib den zweiten Summanden ein: ");
+         string zweiterSummand = Console.ReadLine();
+
+         //  Wandel Text in Ganzzahlen
+         int ersterSummandAlsZahl = Convert.ToInt32(ersterSummand);
+         int zweiterSummandAlsZahl = Convert.ToInt32(zweiterSummand);
+
+         // Berechnung ausführen
+         int summe = ersterSummandAlsZahl + zweiterSummandAlsZahl;
+
+         //Ausgabe
+         Console.WriteLine("Die summe ist: {0}", summe);
+
+         Console.ReadLine();
+      }
+   }
+}
